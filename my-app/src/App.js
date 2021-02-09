1f4db51e3ab05705f28fea9e2bc6d92c4330cea2
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-02-09 15:01:15 -0600

    "Have an error, apparently index.js does not exist on my disk. Going to see if I can fix it in Git, if not, I will start all over again."

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios"
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      placeholder: "",
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/chance10113")
+      .then((res) => {
+        console.log("res", res)
+        console.log("res.data", res.data)
+      })
+      .catch((error) => {
+        console.log("error", error)
+      })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>
+          {" "}
+          U<br></br>N<br></br>D<br></br>E<br></br>R<br></br> <br></br>C<br></br>
+          O<br></br>N<br></br>S<br></br>T<br></br>R<br></br>U<br></br>C<br></br>
+          T<br></br>I<br></br>O<br></br>N{" "}
+        </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
