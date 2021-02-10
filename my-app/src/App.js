6032891c7fbf719ec758eae20ecda6da6f9a7fd0
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-02-09 22:27:55 -0600

    Basic Styling is done. Will touch up later.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import axios from "axios";
-// import styled from "styled-components"
 import "./App.css";
 
 class App extends React.Component {
@@ -17,8 +16,8 @@ class App extends React.Component {
       .get("https://api.github.com/users/chance10113")
       .then((res) => {
         // console.log("res", res);
-        console.log("User res.data", res.data);
-        console.log("User res.data.name", res.data.name);
+        // console.log("User res.data", res.data);
+        // console.log("User res.data.name", res.data.name);
         this.setState({
           userCard: res.data,
         });
@@ -30,7 +29,7 @@ class App extends React.Component {
       .get("https://api.github.com/users/chance10113/followers")
       .then((res) => {
         // console.log("res", res)
-        console.log("follower res.data", res.data);
+        // console.log("follower res.data", res.data);
         this.setState({
           followers: res.data,
         });
@@ -44,8 +43,12 @@ class App extends React.Component {
     return (
       <div className="container">
         <h1> GitHub User Card! </h1>
-        <div className="UserCard">
-          <img src={this.state.userCard.avatar_url} alt="Avatar" />
+        <div className="userCard">
+          <img
+            src={this.state.userCard.avatar_url}
+            alt="Avatar"
+            className="userImg"
+          />
           <div className="cardInfo">
             <h3>{this.state.userCard.name}</h3>
             <p>{this.state.userCard.login}</p>
@@ -58,7 +61,7 @@ class App extends React.Component {
             </p>
             <p>Followers: {this.state.userCard.followers}</p>
             <p>Following: {this.state.userCard.following}</p>
-            <p>Bio: {this.state.userCard.bio}</p>
+            {/* <p>Bio: {this.state.userCard.bio}</p> */}
           </div>
         </div>
         <h2>Followers</h2>
@@ -67,20 +70,13 @@ class App extends React.Component {
             <div className="followerCard" key={follower.id}>
               <img src={follower.avatar_url} alt="Avatar" />
               <div className="cardInfo">
-                <h3>{follower.name}</h3>
-                <p>{follower.login}</p>
+                <h3>{follower.login}</h3>
                 <p>{follower.location}</p>
                 <p>
                   Profile: <a href={follower.html_url}>{follower.html_url}</a>
                 </p>
-                <p>
-                  Followers:{" "}
-                  <a href={follower.followers_url}>{follower.followers_url}</a>
-                </p>
-                <p>
-                  Following:{" "}
-                  <a href={follower.following_url}>{follower.following_url}</a>
-                </p>
+                <p>Followers:{follower.followers_url}</p>
+                <p>Following:{follower.following_url}</p>
               </div>
             </div>
           ))}
@@ -91,9 +87,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-// Styled Components
-
-// const Container = styled.div`
-//   text-align: center
-// `
