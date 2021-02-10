e633d11f5690c33ebe66f79e84ded48ca654195b
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-02-09 22:02:55 -0600

    Started Styling, but it doesn't like styled-components. Also, it tries to stage changes from node modules.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+// import styled from "styled-components"
 import "./App.css";
 
 class App extends React.Component {
@@ -25,18 +26,18 @@ class App extends React.Component {
       .catch((error) => {
         console.log("userCard axios error", error);
       });
-      axios
+    axios
       .get("https://api.github.com/users/chance10113/followers")
       .then((res) => {
         // console.log("res", res)
-        console.log("follower res.data", res.data)
+        console.log("follower res.data", res.data);
         this.setState({
-          followers: res.data
-        })
+          followers: res.data,
+        });
       })
       .catch((error) => {
-        console.log("follower axios error", error)
-      })
+        console.log("follower axios error", error);
+      });
   }
 
   render() {
@@ -44,32 +45,44 @@ class App extends React.Component {
       <div className="container">
         <h1> GitHub User Card! </h1>
         <div className="UserCard">
-        <img src={this.state.userCard.avatar_url} alt="Avatar"  />
+          <img src={this.state.userCard.avatar_url} alt="Avatar" />
           <div className="cardInfo">
-        <h3>{this.state.userCard.name}</h3>
-        <p>{this.state.userCard.login}</p>
-        <p>{this.state.userCard.location}</p>
-        <p>Profile: <a href={this.state.userCard.html_url}>{this.state.userCard.html_url}</a></p>
-        <p>Followers: {this.state.userCard.followers}</p>
-        <p>Following: {this.state.userCard.following}</p>
-        <p>Bio: {this.state.userCard.bio}</p>
+            <h3>{this.state.userCard.name}</h3>
+            <p>{this.state.userCard.login}</p>
+            <p>{this.state.userCard.location}</p>
+            <p>
+              Profile:{" "}
+              <a href={this.state.userCard.html_url}>
+                {this.state.userCard.html_url}
+              </a>
+            </p>
+            <p>Followers: {this.state.userCard.followers}</p>
+            <p>Following: {this.state.userCard.following}</p>
+            <p>Bio: {this.state.userCard.bio}</p>
           </div>
         </div>
         <h2>Followers</h2>
         <div className="followers">
           {this.state.followers.map((follower) => (
-             <div className="UserCard">
-             <img src={follower.avatar_url} alt="Avatar"  />
-               <div className="cardInfo">
-             <h3>{follower.name}</h3>
-             <p>{follower.login}</p>
-             <p>{follower.location}</p>
-             <p>Profile: <a href={follower.html_url}>{follower.html_url}</a></p>
-             <p>Followers: {follower.followers}</p>
-             <p>Following: {follower.following}</p>
-             <p>Bio: {follower.bio}</p>
-               </div>
-             </div>
+            <div className="followerCard" key={follower.id}>
+              <img src={follower.avatar_url} alt="Avatar" />
+              <div className="cardInfo">
+                <h3>{follower.name}</h3>
+                <p>{follower.login}</p>
+                <p>{follower.location}</p>
+                <p>
+                  Profile: <a href={follower.html_url}>{follower.html_url}</a>
+                </p>
+                <p>
+                  Followers:{" "}
+                  <a href={follower.followers_url}>{follower.followers_url}</a>
+                </p>
+                <p>
+                  Following:{" "}
+                  <a href={follower.following_url}>{follower.following_url}</a>
+                </p>
+              </div>
+            </div>
           ))}
         </div>
       </div>
@@ -78,3 +91,9 @@ class App extends React.Component {
 }
 
 export default App;
+
+// Styled Components
+
+// const Container = styled.div`
+//   text-align: center
+// `
